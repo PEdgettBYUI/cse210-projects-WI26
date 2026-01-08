330494a83a6c7472b45fe26ef018b10042fbe845
ContraZombie4 <gameking599@gmail.com>
2026-01-08 13:37:20 -0700

    First Commit/Test Commit;

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("The Strangeness of The World Can Amuse and Frighten. Take Care!");
     }
 }
