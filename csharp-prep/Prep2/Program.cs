b1b5265d54e2b1b226bc6bda94d9ec32b60d8477
ContraZombie4 <102491665+ContraZombie4@users.noreply.github.com>
2026-01-11 21:48:54 -0700

    Completed Prep 2 - "Grade Checker" Excercise;

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,49 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Welcome to the Score to Letter Grade Converter!");
+
+        // Initialize Variables
+        int num = 0;
+        string letter;
+
+        Console.Write("Input your score (0-100): ");
+        string userInput = Console.ReadLine();
+        num = int.Parse(userInput);
+
+        // Determine letter grade
+        if (num >= 90)
+        {
+            letter = "A";
+        }
+        else if (num >= 80)
+        {
+            letter = "B";
+        }
+        else if (num >= 70)
+        {
+            letter = "C";
+        }
+        else if (num >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your final grade is {letter}.");
+
+        // Pass check
+
+        if (num >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("You Failed. Try again next semester");
+        }
     }
 }
