38c15afbdf006ed19706bcbdac909ecd21d77f96
ContraZombie4 <gameking599@gmail.com>
2026-01-15 13:39:31 -0700

    Sandbox examples.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,10 +6,25 @@ class Program
     {
         Console.WriteLine("Hello Sandbox World!");
 
-        int num = 96;
-        int ones = 0;
+        // int num = 96;
+        // int ones = 0;
 
-        ones = num % 10;
-        Console.Write(ones);
+        // ones = num % 10;
+        // Console.Write(ones);
+
+        // Used for error handling. Programs in a "safe" response to an error
+
+
+        try
+        {
+            int numerator = 10;
+            int denominator = 0;
+            int result = numerator/denominator;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Ope, something happened.");
+            Console.WriteLine(e.ToString());    // Prints the error message to the console.
+        }
     }
 }
