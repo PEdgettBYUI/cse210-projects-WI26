e9c4b7a8d0a2e120b8c8952cddafd4ae1585d753
Patrick Edgett <edg24004@byui.edu>
2026-01-20 23:03:48 -0700

    cs-prep5 completed;

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,78 @@
 using System;
 
+// This program is a demonstration of Functions in C#
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        // Structure of a function reminder:
+        // returnType FunctionName(dataType parameter1, dataType parameter2)
+        // {
+        //      // Function body
+        // }
+
+        Console.WriteLine("Hello Prep5 World!\n");
+        
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+
+        int squaredNum = SquareNumber(userNumber);
+
+        int birthYear;
+
+        PromptUserBirthYear(out birthYear);
+
+        DisplayResult(userName, squaredNum, birthYear);
+
+        Console.WriteLine();
+        
+}
+
+static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
     }
+
+    static string PromptUserName()
+    {
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Enter your favorite number: ");
+        string favNum = Console.ReadLine();
+
+        return int.Parse(favNum);
+    }
+
+// Example of "Pass by Reference" function
+// Sample Solution referenced;
+// NOTE: Look into more later, what even is going on here????
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        Console.Write($"Please enter the year you were born: ");
+        birthYear = int.Parse(Console.ReadLine());
+    }
+
+    static int SquareNumber(int num)
+    {
+        return num * num;
+    }
+
+    static void DisplayResult(string name, int square, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+        Console.WriteLine($"{name}, you will turn {2025 - birthYear} this year.");
+    }
+
+
+    
+
 }
