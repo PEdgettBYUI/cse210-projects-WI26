85d6ee1808d36401aa67751c6b97a0974f7d7a46
Patrick Edgett <edg24004@byui.edu>
2026-01-31 22:31:52 -0700

    Entry.cs completed;

--- prove/Develop03/Entry.cs
+++ prove/Develop03/Entry.cs
@@ -1,3 +1,6 @@
+using System.Data;
+using System.IO.Compression;
+
 class Entry
 {
     private string _date;
@@ -7,8 +10,23 @@ class Entry
     private string _response;
 
     // Read File
+    public void EntryFromFile(string e_date, string e_prompt, string e_response)
+    {
+        _date = e_date;
+        _prompt = e_prompt;
+        _response = e_response;
+    }
 
     // Set variables and Initialize Entry
+    public void NewEntry()
+    {
+        setDate();
+        setPrompt();
+
+        Console.WriteLine(_date);
+        Console.WriteLine(_prompt);
+        recordResponse();
+    }
 
     // List of Prompts
        private List<string> prompts = new List<string>
@@ -24,10 +42,38 @@ class Entry
         "Did you see anyone interesting today?"
     };
 
+    // Set Date for the current entry
+    public void setDate()
+    {
+        DateTime currentDate = DateTime.Now;    // Retrieves in format (y/m/d)
+        string dateAsString = currentDate.ToString("MMMM dd, yyyy");
+        _date = dateAsString;
+        // Console.WriteLine(dateAsString);
+    }
+
     // Retrieve prompt from List
+    public void setPrompt()
+    {
+        Random randomQuestion = new Random();
+        string chosenPrompt = prompts[randomQuestion.Next(prompts.Count)];
+        _prompt = chosenPrompt;
+        // Console.WriteLIne(chosenPrompt);
+    }
 
     // Record Entry Response to Prompt
+    public void recordResponse()
+    {
+        Console.Write(">  ");
+        _response = Console.ReadLine();
+    }
 
     // Turn Class into a String for Storage in a .txt file
-
+    public string convertEntryString()
+    {
+        string outputString = "";
+        outputString = $"{_date}#{_prompt}#{_response}"; 
+        // Console.WriteLine(outputString);
+        return outputString;
+    }
+ 
 }
