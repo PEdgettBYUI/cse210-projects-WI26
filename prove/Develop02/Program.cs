cecf1ca9932a9f50efb29ff7500bdb08c6fa3893
Patrick Edgett <edg24004@byui.edu>
2026-01-31 23:48:12 -0700

    Moved files from Develop03 to Develop02 after completing debugging;

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,49 @@
+/*
+Patrick T. Edgett
+CSE210 - Prog W/ Classes
+WI26
+--------------------------------
+This program is a Jouraling App. It consists of 4 files:
+Program.cs (here), Entry.cs, Journal.cs, & Menu.cs
+*/
+
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        // Initialize classes
+        Menu mainMenu = new Menu();
+        Journal jtest = new Journal();
+        Entry test = new Entry();  // TEST Entry
+
+        // Initialize variables for Main
+        int userChoice = 0;
+
+        // Introduction to the program
+        Console.WriteLine("Welcome to YourJourn, your personal Journaling Program.\n");
+
+
+        while (userChoice != 5)
+        {
+            Console.WriteLine("----YourJourn----");    // Trying out having the name of the program display above the menu
+            mainMenu.displayMenu();
+            Console.Write("Choose: ");
+
+            //Makes sure that the User's Input is valid
+            string input = Console.ReadLine();
+            if (!int.TryParse(input, out userChoice))
+            {
+                Console.WriteLine("Please enter a valid number.");
+                continue; // Skip to next loop iteration
+            }
+
+            mainMenu.parseUserChoice(userChoice, test, jtest);
+            if (userChoice == 5)
+            {
+                Console.WriteLine("[CLOSING program...]\n[Goodbye!]\n");
+            }
+        }
     }
 }
